fd53e003a3eb98fbbf2caabb7987fa63f2860a6d
elsangallo <71708891+elsangallo@users.noreply.github.com>
2022-01-21 22:40:12 +0100

    Abschlussprojekt
    - CSS erstellt und implementiert
    - div. finale Anpassungen
    
    @bewerter: ich habe heute das css geschrieben und versucht einzupflegen. Zum Ausprobieren habe ich es erstmal in den Template-Ordner getan und mit den HTMLs getestet, wo es funktionierte. Nach dem verschieben in den Static-Ordner und dem Einbinden ins Python funktionierte es nicht mehr. Trotz verschiedener Forumseinträge gelang es mir nicht dies zu beheben.

--- Banic/test_bankkonto.py
+++ Banic/test_bankkonto.py
@@ -8,13 +8,13 @@ konto2 = 0
 konto3 = 0
 konto4 = 0
 
-#runs on http://localhost:5001/bank
-app = Flask("Bankkonto") # die flask-app "Bankkonto" wird erstellt
-
+#runs on http://localhost:5000/bank
+app = Flask("Bankkonto")# die flask-app "Bankkonto" wird erstellt
+app.static_folder = 'static'
 
 @app.route('/bank') # der url-link für die app wird erstellt, die url mit der endung "bank" führt dann die untenstehende funktion aus
 def bank(): # die funktion bank() führt die funktion render_template aus, womit das html-template ausgeführt wird, welches im file index.html erstellt wurde
-    return render_template('index.html', name="Elias") # im index.html wird der wert für die variable nicht festgelegt, dass wird hier mit name="Elias" gemacht, dadurch lässt es sich leichter anpassen
+    return render_template('index.html') # im index.html wird der wert für die variable nicht festgelegt, dass wird hier mit name="Elias" gemacht, dadurch lässt es sich leichter anpassen
 
 
 @app.route('/bank/ein_auszahlen', methods=['GET', 'POST']) # die url ein_auszahlen wird erstellt
@@ -24,7 +24,8 @@ def ein_auszahlen(): # die funktion der url wird erstellt, wenn man draufklickt 
         betrag = int(request.form['betrag']) # der betrag wird aus dem formularfeld betrag abgefragt
         konto = 'konto' + kontonummer # variable konto wird mit dem wort konto und der eingegebenen kontonummer gefüllt
         globals()[konto] = globals()[konto] + betrag # der neue kontostand wird berechnet indem die globale variable aufgerufen wird
-        return 'Der neue Kontostand von ' + konto + ' ist jetzt ' + str(globals()[konto]) # der neue kontostand wird ausgegeben
+        ausgabe1 = 'Der neue Kontostand von ' + konto + ' ist jetzt ' + str(globals()[konto])
+        return render_template('ausgabeeinauszahlen.html', ausgabe1=ausgabe1) # der neue kontostand wird ausgegeben
     else:
         return render_template('ein_auszahlen.html') # das template ein-auszahlen wird aufgerufen
 
@@ -39,9 +40,9 @@ def uebertragen(): # die funktion der url wird erstellt, wenn man draufklickt er
         konto_zu = 'konto' + kontonummer_zu # variable konto_zu wird mit dem wort konto und der eingegebenen kontonummer des empfängerkontos gefüllt
         globals()[konto_von] = globals()[konto_von] - betrag # der neue kontostand wird berechnet indem die globale variable aufgerufen wird
         globals()[konto_zu] = globals()[konto_zu] + betrag # der neue kontostand wird berechnet indem die globale variable aufgerufen wird
-        answer1 = 'Der Kontostand von ' + konto_von + ' ist neu ' + str(globals()[konto_von]) # die variable answer1 wird mit einem satz und der variable konto_von gefüllt zuzüglich wird die globale varibale abgerufen unnd eingefügt
-        answer2 = 'Der Kontostand von ' + konto_zu + ' ist neu ' + str(globals()[konto_zu]) # die variable answer2 wird mit einem satz und der variable konto_zu gefüllt zuzüglich wird die globale varibale abgerufen unnd eingefügt
-        return answer1 + '. ' + answer2 # die variable answer1 und answer2 werden zusammengefügt und ausgegeben
+        answer1 = 'Der Kontostand von ' + konto_von + ' ist neu ' + str(globals()[konto_von]) + '.' # die variable answer1 wird mit einem satz und der variable konto_von gefüllt zuzüglich wird die globale varibale abgerufen unnd eingefügt
+        answer2 = 'Der Kontostand von ' + konto_zu + ' ist neu ' + str(globals()[konto_zu]) + '.' # die variable answer2 wird mit einem satz und der variable konto_zu gefüllt zuzüglich wird die globale varibale abgerufen unnd eingefügt
+        return render_template('ausgabeuebertragen.html', answer1=answer1, answer2=answer2) # die variable answer1 und answer2 werden zusammengefügt und ausgegeben
     else:
         return render_template('uebertragen.html') # das template üebertragen wird abgerufen
 
@@ -51,9 +52,10 @@ def kontostand(): # die funktion der url wird erstellt, wenn man draufklickt ers
     if request.method == 'POST': # falls mit der methode post etwas von der webseite ausgegeben wird werden nachfolgende zeilen ausgeführt
         kontonummer = request.form['kontonummer'] # die kontonummer wird aus dem formularfeld kontonummer abgefragt und als variable kontonummer gespeichert
         konto = 'konto' + kontonummer # variable konto wird mit dem wort konto und der eingegebenen kontonummer gefüllt
-        return str(globals()[konto]) # die globale variable wird ausgegeben
+        ausgabe2 = 'Der aktuelle Kontostand von ' + konto + ' ist ' + str(globals()[konto]) + '.'
+        return render_template('ausgabekontostand.html', ausgabe2=ausgabe2) # die globale variable wird ausgegeben
     else:
         return render_template('kontostand.html') # das template kontostand wird abgerufen
 
 if __name__ == "__main__":
-    app.run(debug=True, port=5001) # hiermit wird festgelegt, dass wenn die app startet das debugging ein und der port 5001 abgerufen wird
+    app.run(debug=True, port=5000) # hiermit wird festgelegt, dass wenn die app startet das debugging ein und der port 5001 abgerufen wird
